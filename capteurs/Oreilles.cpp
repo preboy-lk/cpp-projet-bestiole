337f02a5badc1d0d1925836537f58437cd35f1ae
Heliote225 <coolheliote@gmail.com>
2024-03-26 01:58:06 +0100

    feat: ears drawings added!

--- capteurs/Oreilles.cpp
+++ capteurs/Oreilles.cpp
@@ -4,7 +4,16 @@ std::string Oreilles::getNom() const {
     return "Oreilles";
 }
 void Oreilles::draw(int x,int y, double size,double orientation,UImg & support, T* couleur) const {
-    //support.draw_circle( x, y, audibleDistance, couleur );
+    double xt = x + cos( orientation )*size/2.1;
+    double yt = y - sin( orientation )*size/2.1;
+    double angle = (45/180.0)*M_PI;
+    double xb = xt - size * cos(-orientation + angle);
+    double yb = yt - size * sin(-orientation + angle);
+    double xc = xt - size * cos(-orientation - angle);
+    double yc = yt - size * sin(-orientation - angle);
+    T earColor[] = {0, 0, 0};//couleur noire pour les oreilles
+    support.draw_line(xb, yb, xc, yc, earColor, 2.0f);
+    //std::cout << "Drawed Oreilles" << std::endl;
 }
 
 double Oreilles::getAudibleDistance()
