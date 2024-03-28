d47676db8e274be75e92094f6b147607a0740c20
Heliote225 <coolheliote@gmail.com>
2024-03-28 01:06:48 +0100

    fix: change ratio for personalites multiples!

--- capteurs/Yeux.cpp
+++ capteurs/Yeux.cpp
@@ -4,24 +4,22 @@ std::string Yeux::getNom() const {
     return "Yeux";
 }
 void Yeux::draw(int x,int y, double size,double orientation,UImg & support, T* couleur, float opacite ) const {
-    double angle = (20/180.0)*M_PI;
+    /*double angle = (20/180.0)*M_PI;
     double xb = x + size * cos(-orientation + angle)/2.5;
     double yb = y + size * sin(-orientation + angle)/2.5;
     double xc = x + size * cos(-orientation - angle)/2.5;
     double yc = y + size * sin(-orientation - angle)/2.5;
     T eyeColor[] = {0, 0, 0};//couleur noire pour les yeux
     support.draw_circle(xb, yb,size/6, eyeColor);
-    support.draw_circle(xc, yc,size/6, eyeColor);
+    support.draw_circle(xc, yc,size/6, eyeColor);*/
     //std::cout << "Drawed Yeux" << std::endl;
-    // double xq = x;
-    // double yq = y;
-    // double angle = visionAngle/2;
-    // double xb = xq + visionDistance * cos(orientation - angle);
-    // double yb = yq - visionDistance * sin(orientation - angle);
-    // double xc = xq + visionDistance * cos(orientation + angle);
-    // double yc = yq - visionDistance * sin(orientation + angle);
-    // float opacite = 0.3;
-    // support.draw_triangle( xq, yq, xb, yb, xc, yc, couleur );
+    double angle = visionAngle/2;
+    double xb = x + visionDistance * cos(orientation - angle);
+    double yb = y - visionDistance * sin(orientation - angle);
+    double xc = x + visionDistance * cos(orientation + angle);
+    double yc = y - visionDistance * sin(orientation + angle);
+    float opacity = 0.3f;
+    support.draw_triangle( x, y, xb, yb, xc, yc, couleur, opacity );
 }
 
 double Yeux::getAudibleDistance()
